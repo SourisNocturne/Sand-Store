c807d25e7498c1d9a572e52625363e86f944a447
Picsou <118018749+SourisNocturne@users.noreply.github.com>
2023-01-07 19:31:42 +0100

    Add files via upload

---
+++ formulaire.php
@@ -0,0 +1,53 @@
+<!Doctype php>
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<?php
+
+  if(isset($_POST['valider']))
+  {
+  	if(isset($_POST['title']) AND isset($_POST['email']) AND isset($_POST['message']))
+  	{
+  		if(!empty($_POST['title']) AND !empty($_POST['email']) AND !empty($_POST['message']))
+  		{
+           $title=htmlspecialchars($_POST['title']);
+           $email=htmlspecialchars($_POST['email']);
+           $message=htmlspecialchars($_POST['message']);
+             
+             echo " merci pour votre email:<mark><b>  $email </b></mark>, nous lirons votre  message dans les plus brefs délai: </br></br> $message </h2>";
+  		}
+  	}
+  }
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <img class="image6" src="https://www.cdiscount.com/pdt2/7/1/8/1/700x700/dul3031520129718/rw/peinture-creme-de-couleur-2-5-l-poivre.jpg" width="1275" height="300">
+  <img class="image1" src="https://www.cdiscount.com/pdt2/7/1/8/1/700x700/dul3031520129718/rw/peinture-creme-de-couleur-2-5-l-poivre.jpg" width="1250" height="90">
+  <meta charset="utf-8">
+  <HR width="1050px""><title>Sand Store</title></HR>
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+  <link rel="shortcut icon" type="image/x-icon" href="//dka575ofm4ao0.cloudfront.net/pages-favicon_logos/original/338788/A736EB22-A39A-427F-B018-53D24EBE7949.png">
+
+</head>
+    <body class="body1">
+        <font face="Arial Black"> 
+            <h1 class="welcome">
+            Sand Store
+            </h1>  
+       </font> <p class="z2">©𝗦𝗮𝗻𝗱 𝗦𝘁𝗼𝗿𝗲</p>
+       <h2 class="j3" href="#" style="--clr:#ff22bb;--i:0;"><a class="liena" href="home.html" style="text-decoration:none">Home</a></h2>
+       <h2 class="j2" href="#" style="--clr:#ff22bb;--i:0;"><a class="lien2" href="Contact.html" style="text-decoration:none">Contact</a></h2>
+       <h2 class="j4" href="#" style="--clr:#ff22bb;--i:0;"><a class="lienb" href="Products.html" style="text-decoration:none">Products</a></h2>
+       <h2 class="j5" href="#" style="--clr:#ff22bb;--i:0;"><a class="lienc" href="Payment Methods.html" style="text-decoration:none">Payment Methods</a></h2>
+      <img class="r1" src="https://media.discordapp.net/attachments/817431990969565244/1060564700531798086/6485FC3A-1FA9-445A-9CFC-02FCB1BB03F2_1.gif" width="60px">
+      <p class="j1">𝗦𝗮𝗻𝗱 𝗦𝘁𝗼𝗿𝗲</p>  <p class="espac">eghrthhhyyh</p>
+      <font face="Arial Black"> <h2 class="Tabsa8">Tabs</h2></font>
+      <font face="Arial Black"> <h2 class="Sociala8">Social</h2></font>
+      <h2 class="tabsa1" style="--clr:#ff22bb;--i:0;"><a class="liena2" href="home.html" style="text-decoration:none">Home</a></h2>
+      <h2 class="tabsa2" style="--clr:#ff22bb;--i:0;"><a class="lienb2" href="Payment Methods.html" style="text-decoration:none">Payment Methods</a></h2>
+      <h2 class="tabsa3" style="--clr:#ff22bb;--i:0;"><a class="lien1" href="Contact.html" style="text-decoration:none">Contact</a></h2>
+      <h2 class="tabsa4" style="--clr:#ff22bb;--i:0;"><a class="lienc2" href="Products.html" style="text-decoration:none">Products</a></h2>
+      <h2 class="sociala1" style="--clr:#ff22bb;--i:0;"><a class="liens2" href="https://discord.gg/sandstore" target="_blank" style="text-decoration:none">Discord</a></h2>
+      <h2 class="sociala2" style="--clr:#ff22bb;--i:0;"><a class="liens3" href="https://youtu.be/U3E9Eusspic" target="_blank" style="text-decoration:none">Youtube</a></h2>
+      <img class="image8" src="https://www.cdiscount.com/pdt2/7/1/8/1/700x700/dul3031520129718/rw/peinture-creme-de-couleur-2-5-l-poivre.jpg" width="720" height="450">
+</body></html>
